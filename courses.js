10a0a5a0566e45566469ca360e8f345ad008f0ef
bisherahal <126331544+bisherahal@users.noreply.github.com>
2026-03-13 14:45:23 +0300

    Add files via upload

--- courses.js
+++ courses.js
@@ -395,7 +395,7 @@ function submitEnrollment() {
 
 function _launchConfetti() {
   const canvas = document.createElement("canvas");
-  canvas.style.cssText = "position:fixed;top:0;left:0;width:100%;height:100%;pointer-events:none;z-index:9999";
+  canvas.style.cssText = "position:fixed;top:0;left:0;width:100%;height:100%;pointer-events:none;z-index:999999";
   document.body.appendChild(canvas);
   const ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
